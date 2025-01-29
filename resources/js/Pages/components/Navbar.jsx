8c80064c4dc8dcb22adbe1e87910dc7b4bbf9378
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-01-29 12:12:08 +0700

    adding underline hover effect in navbar

--- resources/js/Pages/components/Navbar.jsx
+++ resources/js/Pages/components/Navbar.jsx
@@ -4,16 +4,16 @@ import React from "react";
 export default function Navbar(){
     return(
         <div className="absolute hidden sm:flex gap-6 text-2xl md:mr-7 lg:mr-10 mt-1 top-2 right-0 font-jua text-white">
-            <Link href="/">
+            <Link href="/" className="relative before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300">
                 home
             </Link>
-            <Link href="#about">
+            <Link href="#about" className="relative before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300">
                 about
             </Link>
-            <Link href="/">
+            <Link href="/" className="relative before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300">
                 product
             </Link>
-            <Link href="/">
+            <Link href="/" className="relative before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300">
                 review
             </Link>
         </div>
