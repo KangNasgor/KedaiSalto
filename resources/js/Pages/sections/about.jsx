4e64cfba660a174bb814c5f00151e1c14de81c03
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-01-30 22:25:34 +0700

    making the about page responsive to tablet and mobile display

--- resources/js/Pages/sections/about.jsx
+++ resources/js/Pages/sections/about.jsx
@@ -18,15 +18,15 @@ export default function About() {
         2: <MashedPotato prev={onCarouselPrev} next={onCarouselNext}/>,
     };
     return (
-        <div className='h-screen bg-[#FFB42D] py-10 flex items-center' id='about'>
-            <div className='w-6/12'>
-                <div className='pl-20 w-10/12'>
+        <div className='h-fit lg:h-screen bg-[#FFB42D] py-24 flex flex-col-reverse lg:flex-row items-center' id='about'>
+            <div className='w-full lg:w-6/12'>
+                <div className='lg:pl-20 mx-auto w-10/12'>
                     {product[activeProduct]}
                 </div>
             </div>
-            <div className='w-6/12 px-7'>
-                <div className='w-10/12 bg-[#FF2E2E] px-5 py-4 rounded-2xl'>
-                    <p className='font-jua text-[#FFB42D] text-3xl'>
+            <div className='w-full md:w-6/12 px-7 mb-16 lg:mb-0'>
+                <div className='md:w-full lg:w-10/12 bg-[#FF2E2E] px-5 py-4 rounded-2xl'>
+                    <p className='font-jua text-[#FFB42D] text-3xl text-center'>
                         Nikmati setiap gigitan dan tegukan dengan cita rasa khas yang menggugah selera, dibuat dengan bahan pilihan untuk pengalaman kuliner yang tak terlupakan.
                     </p>
                 </div>
@@ -38,16 +38,20 @@ export default function About() {
 function EsLumut({prev, next}) {
     return (
         <div>
-            <div className='flex items-center gap-5 relative'>
-                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
+            <div className='flex items-center gap-5 h-fit relative'>
+                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute hidden md:block -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
                 <div className='bg-eslumut h-56 w-full rounded-2xl bg-cover bg-center mb-8'></div>
-                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
+                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute hidden md:block -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
             </div>
-            <h1 className='text-[#FF2E2E] font-jua text-3xl mb-1'>Es Lumut</h1>
-            <p className='text-[#FF2E2E] font-jua text-lg mb-5'>
+            <div className='flex gap-5 justify-around mb-5'>
+                <FontAwesomeIcon icon={faArrowLeft} className='text-xl block md:hidden hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
+                <FontAwesomeIcon icon={faArrowRight} className='text-xl block md:hidden hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
+            </div>
+            <h1 className='text-[#FF2E2E] text-center md:text-left font-jua text-3xl mb-1'>Es Lumut</h1>
+            <p className='text-[#FF2E2E] text-center md:text-left font-jua text-lg mb-5'>
                 Minuman segar dengan tekstur unik dan rasa manis yang pas, menghadirkan sensasi lembut dan menyegarkan di setiap tegukan.
             </p>
-            <Link href="/" className='font-jua bg-[#FF2E2E] text-white px-4 py-3 rounded-lg'>
+            <Link href="/" className='font-jua bg-[#FF2E2E] text-white px-4 py-3 rounded-lg block mx-auto w-fit'>
                 BUY NOW
             </Link>
         </div>
@@ -57,9 +61,9 @@ function Dimsum({prev, next}) {
     return (
         <div>
             <div className='flex items-center gap-5 relative'>
-                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
+                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute hidden md:block -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
                 <div className='bg-dimsum h-56 w-full rounded-2xl bg-cover bg-center mb-8'></div>
-                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
+                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute hidden md:block -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
             </div>
             <h1 className='text-[#FF2E2E] font-jua text-3xl mb-1'>Dimsum</h1>
             <p className='text-[#FF2E2E] font-jua text-lg mb-5'>
@@ -75,9 +79,9 @@ function MashedPotato({prev, next}) {
     return (
         <div>
             <div className='flex items-center gap-5 relative'>
-                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
+                <FontAwesomeIcon icon={faArrowLeft} className='text-xl absolute hidden md:block -left-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={prev}/>
                 <div className='bg-mashpotato h-56 w-full rounded-2xl bg-cover bg-center mb-8'></div>
-                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
+                <FontAwesomeIcon icon={faArrowRight} className='text-xl absolute hidden md:block -right-10 hover:scale-125 active:scale-110 transition-all duration-200' onClick={next}/>
             </div>
             <h1 className='text-[#FF2E2E] font-jua text-3xl mb-1'>Mashed Potatoes</h1>
             <p className='text-[#FF2E2E] font-jua text-lg mb-5'>
