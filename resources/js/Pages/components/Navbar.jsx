8d5ce7e07bbb898a786bb9f7420760b704864e16
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-02-22 14:46:07 +0700

    finishing testimonial page and adding background on navbar when scrolled

--- resources/js/Pages/components/Navbar.jsx
+++ resources/js/Pages/components/Navbar.jsx
@@ -1,27 +1,39 @@
 import { Link, usePage } from "@inertiajs/inertia-react";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 
 export default function Navbar() {
-    const url  = window.location.pathname;
-
+    const url = window.location.pathname;
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0)
+        }
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [])
     return (
-        <div className="fixed w-full justify-end hidden sm:flex pt-3 gap-6 text-2xl md:mr-7 lg:pr-10 font-jua">
-            <Link href="/" className={`relative ${url === "/" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
-                home
-            </Link>
-            <Link href="/user/product" className={`relative ${url === "/user/product" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
-                product
-            </Link>
-            <Link href="/user/cart" className={`relative ${url === "/user/cart" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
-                keranjang
-            </Link>
-            <Link href="/user/order" className={`relative ${url === "/user/order" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
-                order
-            </Link>
-            <Link href="/user/account" className={`relative ${url === "/user/account" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
-                account
-            </Link>
+        <div className={`fixed z-50 w-full justify-between hidden sm:flex py-4 text-2xl md:mr-7 lg:pr-10 font-jua transform transition-all duration-300 ${isScrolled > 0 ? "bg-[#FF2E2E] shadow-xl" : "bg-none shadow-none"}`}>
+            <div className="pl-3">
+                <h1 className='text-white font-jua text-2xl sm:mt-1 w-fit'>Kedai Salto</h1>
+            </div>
+            <div className="flex gap-6">
+                <Link href="/" className={`relative ${url === "/" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
+                    home
+                </Link>
+                <Link href="/user/product" className={`relative ${url === "/user/product" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
+                    product
+                </Link>
+                <Link href="/user/cart" className={`relative ${url === "/user/cart" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
+                    keranjang
+                </Link>
+                <Link href="/user/order" className={`relative ${url === "/user/order" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
+                    order
+                </Link>
+                <Link href="/user/account" className={`relative ${url === "/user/account" ? 'text-white' : 'text-white'} before:h-1 before:absolute before:bg-white before:content-[''] before:w-0 before:bottom-0 before:rounded-md before:hover:w-full before:transition-all before:duration-300`}>
+                    account
+                </Link>
+            </div>
         </div>
     );
 }
--- resources/js/Pages/sections/testimonial.jsx
+++ resources/js/Pages/sections/testimonial.jsx
@@ -34,53 +34,89 @@ export default function Testimonial() {
             <div className="w-11/12 overflow-hidden mx-auto bg-[#e69f20] rounded-lg py-10 relative"> {/* Visible Div for user, this is the container of the carousel */}
                 <div className="flex transform transition-all duration-300 carousel-item"> {/** This div contains all carousel children, the div is moved left and right to simulate carousel */}
                     <div className="min-w-full carousel-child">
-                        <div className="bg-[#FF2E2E] mx-auto w-11/12 rounded-md p-5 flex justify-center gap-5">
+                        <div className="bg-[#FF2E2E] mx-auto w-11/12 rounded-md p-5 grid grid-cols-3 justify-center gap-5">
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Yanto</h1>
+                                <img src="/asset/admetal.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">Adi Rabbani</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "Saya selalu jadi pelanggan setia Kedai Salto! Dumpling-nya enak banget, es lumut-nya juga segar. Pokoknya, tempat ini jadi favorit banget deh buat nongkrong sambil nyemil."
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Aeri</h1>
+                                <img src="/asset/jijel.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">GISELLE</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "너무 맛있어요! 다음에 또 먹고 싶어요~" (Ini enak banget! Mau makan lagi lain kali~)
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Rex</h1>
+                                <img src="/asset/prx.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">PRX f0rsaken</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "Bro, Kedai Salto tuh beneran next level! Dumpling-nya legit enak, mashed potatoes-nya juga lembut banget. Kalau lagi butuh energi sebelum main, makanan di sini sih wajib banget! EZ win kalau udah makan di Kedai Salto.""
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="min-w-full carousel-child">
-                        <div className="bg-[#FF2E2E] w-11/12 mx-auto rounded-md p-5 flex justify-center gap-5">
+                        <div className="bg-[#FF2E2E] w-11/12 mx-auto rounded-md p-5 grid grid-cols-3 justify-center gap-5">
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Yizhuo</h1>
+                                <img src="/asset/ningning.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">NINGNING</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "진짜 맛있다! 진짜 최고야~" (Beneran enak banget! Beneran yang terbaik~)
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Darren</h1>
+                                <img src="/asset/darren.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">IShowSpeed</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "YO CHAT! I just tried Kedai Salto and OMG—THIS STUFF IS CRAZY!!!🔥🔥 Dumplings? BUSSIN’. Es lumut? REFRESHING AS HELL! WOOOO!!! 10/10, W TESTIMONIAL, W FOOD, W INDONESIA!!! LET’S GOOOOOO!!!"
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Makarov</h1>
+                                <img src="/asset/jokowi.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">Joko Widodo</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "Kedai Salto ini makanannya enak sekali. Dumpling-nya gurih, es lumut-nya segar. Harganya juga terjangkau, cocok untuk semua kalangan. Saya rasa ini bisa jadi salah satu kuliner favorit masyarakat. Maju terus UMKM Indonesia!"
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="min-w-full carousel-child">
-                        <div className="bg-[#FF2E2E] w-11/12 mx-auto rounded-md p-5 flex justify-center gap-5">
+                        <div className="bg-[#FF2E2E] w-11/12 mx-auto rounded-md p-5 grid grid-cols-3 justify-center gap-5">
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Rabbani</h1>
+                                <img src="/asset/gojosaturujpg.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">M.Yahya Abdurrohim</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "Kedai Salto tuh kayak tempat makan yang nggak pernah gagal bikin happy. Mashed potatoes-nya lembut dan bumbu-nya pas. Harganya juga ramah di kantong, jadi sering mampir."
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Minjeong</h1>
+                                <img src="/asset/winteo.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">WINTER</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "와~ 이거 진짜 대박이에요! 덤플링 너무 촉촉하고 맛있어요. 그리고 에스 루뭇은 진짜 상쾌해요! 다음에 또 먹고 싶어요~ 꼭 다시 올게요! 💙" (Wow~ Ini beneran luar biasa! Dumpling-nya super juicy dan enak banget. Terus es lumut-nya segar banget! Mau makan lagi lain kali~ Pasti bakal datang lagi! 💙)
+                                </p>
                             </div>
                             <div className="bg-white p-3 rounded-md">
-                                <h1 className="font-jua text-black">Zaenal</h1>
+                                <img src="/asset/gw.jpg" className="object-cover object-center w-32 h-32 rounded-full mx-auto" />
+                                <h1 className="font-jua text-[#FF2E2E] w-fit mx-auto text-lg font-semibold tracking-wide">KangNasgor</h1>
+                                <p className="font-jua text-[#FF2E2E] text-center text-sm">
+                                    "Heed my words-KedaiSalto is a place of fleeting warmth in this accursed land, It serveth a feast most delightful, A meal fit for the Ashen One."
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between w-full px-5 absolute top-0 left-1/2 h-full -translate-x-1/2">
-                    <button onClick={onPrevCarousel}>
+                <div className="flex justify-between w-full px-5 absolute top-0 left-1/2 h-full -translate-x-1/2 pointer-events-none">
+                    <button onClick={onPrevCarousel} className="pointer-events-auto">
                         <div>
                             <div className="h-1 w-3 transform -rotate-45 -translate-y-px bg-white rounded-sm"></div>
                             <div className="h-1 w-3 transform rotate-45 translate-y-px bg-white rounded-sm"></div>
                         </div>
                     </button>
-                    <button onClick={onNextCarousel}>
+                    <button onClick={onNextCarousel} className="pointer-events-auto">
                         <div>
                             <div className="h-1 w-3 transform rotate-45 -translate-y-px bg-white rounded-sm"></div>
                             <div className="h-1 w-3 transform -rotate-45 translate-y-px bg-white rounded-sm"></div>
